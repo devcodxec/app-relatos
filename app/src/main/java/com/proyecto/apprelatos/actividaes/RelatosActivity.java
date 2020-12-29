7cab5f938ec112c606f6cbf6a30916302f4d48ee
dennys <dennys.landy@hotmail.com>
2020-12-29 20:32:29 +0100

    Se crea relatos.xml | Configuracion inicial de RelatosActivity #3

--- app/src/main/java/com/proyecto/apprelatos/actividaes/RelatosActivity.java
+++ app/src/main/java/com/proyecto/apprelatos/actividaes/RelatosActivity.java
@@ -1,5 +1,20 @@
 package com.proyecto.apprelatos.actividaes;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.proyecto.apprelatos.R;
+
 public class RelatosActivity  extends AppCompatActivity {
+
+    //Variable para revisar el log
+    private static final String LOG_APP = "APP_RELATOS";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relatos);
+    }
+
+
 }
