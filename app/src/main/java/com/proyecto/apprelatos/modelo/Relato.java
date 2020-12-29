0b05ed028ab851df648118e6001ed6d52d4f10ac
dennys <dennys.landy@hotmail.com>
2020-12-29 23:47:23 +0100

    Se crea un adaptador y se añaden los métodos para listar relatos en ES/EN #3

--- app/src/main/java/com/proyecto/apprelatos/modelo/Relato.java
+++ app/src/main/java/com/proyecto/apprelatos/modelo/Relato.java
@@ -1,4 +1,64 @@
 package com.proyecto.apprelatos.modelo;
 
-public class Relato {
+import java.io.Serializable;
+
+public class Relato implements Serializable {
+
+    private String titulo;
+    private String descripcion;
+    private String imagen;
+    private String idioma;
+
+    public Relato(){
+
+    }
+
+    public Relato(String titulo, String descripcion, String imagen, String idioma){
+        this.titulo=titulo;
+        this.descripcion=descripcion;
+        this.imagen=imagen;
+        this.idioma=idioma;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    @Override
+    public String toString() {
+        return "Relato{" +
+                "titulo='" + titulo + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", imagen='" + imagen + '\'' +
+                ", idioma='" + idioma + '\'' +
+                '}';
+    }
 }
