154895a8c4c51818d6641fcb6b918795a615b545
dennys <dennys.landy@hotmail.com>
2021-01-05 23:49:30 +0100

    Se visualiza el contenido del relato junto con su imagen #4

---
+++ app/src/main/java/com/proyecto/apprelatos/actividades/DescripcionActivity.java
@@ -0,0 +1,41 @@
+package com.proyecto.apprelatos.actividades;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import com.bumptech.glide.Glide;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.proyecto.apprelatos.R;
+
+public class DescripcionActivity extends Activity {
+
+    //Variable para revisar el log
+    private static final String LOG_APP = "APP_RELATOS";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.descripcion);
+
+        TextView titulo= (TextView)findViewById(R.id.DesTituloRelato);
+        TextView descripcion= (TextView)findViewById(R.id.DesRelato);
+        ImageView fotoRelato= (ImageView)findViewById(R.id.DesImgRelato);
+
+        descripcion.setMovementMethod(new ScrollingMovementMethod());
+
+        Intent intent = getIntent();
+        Bundle bundleDatosRest= intent.getExtras();
+
+        if(bundleDatosRest!=null){
+            titulo.setText(bundleDatosRest.getString("tituloRelato"));
+            descripcion.setText(bundleDatosRest.getString("descripcionRelato"));
+            FirebaseStorage firebaseStorage =  FirebaseStorage.getInstance();
+            StorageReference storageReference = firebaseStorage.getReferenceFromUrl(bundleDatosRest.getString("imagenRelato"));
+            Glide.with(DescripcionActivity.this).load(storageReference).into(fotoRelato);
+        }
+    }
+}
